cc0fa9aa2e73ed75f5e26efa19aa3b5347b13a2c
Баранов Єгор <kolllapa.exe@gmail.com>
2026-05-07 10:17:39 +0300

    Add files via upload

---
+++ less_1/script/JS.js
@@ -0,0 +1,48 @@
+// console.log(`Hello, World!`);
+// console.info('Hello, World!#2');
+// console.error('Hello, World!#3');
+// console.warn('Hello, World!#4');
+
+// var myFirstVar;
+// myFirstVar = 10;
+// console.log(myFirstVar);
+//
+// var mySecondVar;
+// mySecondVar = '2';
+// console.log("Variable: " + mySecondVar);
+// console.log(myFirstVar + mySecondVar);
+// console.log(myFirstVar - mySecondVar);
+//
+// var first, second, third, fourth = 2, "4", "5", "6";
+// var int = 10;
+// var float = 10.2;
+// var bool = false;
+// var str = 'nugget';
+// var nothing;
+// nothing = null;
+//
+// let num = 2;
+// console.log(num)
+
+
+// var num = 13
+// console.log(num)
+//
+// const Pi = 3.14
+
+// let a = 21, b = 4;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+// let a = 2, b;
+// b = a++;
+// console.log(a);
+let a = '2', b = '3';
+console.log(a + b);
+
+let name = 'John';
+console.log("Hello " + name + "!");
+console.log(`Hello World!${name}`);
