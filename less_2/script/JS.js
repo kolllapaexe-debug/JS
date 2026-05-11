31e12f03465070c6f112b239476361430e67dbae
Баранов Єгор <kolllapa.exe@gmail.com>
2026-05-11 13:51:40 +0300

    Add files via upload

---
+++ less_2/script/JS.js
@@ -0,0 +1,60 @@
+// if(variable){
+//     [if true]:
+//     [if true]:
+//     [if true]
+// }
+// else if () {
+//     [if elif]:
+//     [if elif]:
+//     [if elif]:
+// }
+// else() {
+//     [if false]:
+//     [if false]:
+//     [if false]:
+// }
+// let a =3, b ="3"
+// console.log(a == b)
+// console.log(a === b)
+// if(a>b){
+//     c = "a > b"
+// }else if (a < b){
+//     c = "a < b"
+// }else {
+//     c = "a == b"
+// }
+// let a = prompt("enter first num: ")
+// let b = prompt("enter second num: ")
+// let c;
+// if (a > b) c = "a > b";
+// else if (a < b) c = "a < b";
+// else c = "a == b";
+//
+// alert(c)
+// let course= prompt(), title;
+// switch(course) {
+//     case 'figma':
+//     case 'human':
+//     case 'human_medium':
+//     case 'lil puh':
+//         title = 'web development';
+//         break;
+//     case 'Python':
+//         title = 'Programming';
+//         break;
+//     case 'front end':
+//         title = 'web progrmmain';
+//         break;
+//     default:
+//         title = 'No search results found';
+//         break;
+// }
+// alert(title);
+
+let a = false
+let b = false
+let c = true
+console.log(!a);
+console.log(!!a);0
+console.log(a && c);
+console.log(a||b);
